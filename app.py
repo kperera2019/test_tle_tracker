e11d96556ed3b7a18f541ecc15f42845066a6bd5
kperera2019 <kevin.perera13@gmail.com>
2026-05-07 15:02:08 -0400

    Basic API for streaming up

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+import requests
+import httpx
 app = Flask(__name__)
 
 QUERY_URL="https://api.wheretheiss.at/v1/satellites/25544"
@@ -8,3 +9,8 @@ QUERY_URL="https://api.wheretheiss.at/v1/satellites/25544"
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/query")
+def query():
+    response = requests.get(QUERY_URL)
+    return response.json()
--- query.py
+++ query.py
@@ -1,20 +1,21 @@
 import json
-import httpx
-
-URL = "http://127.0.0.1:8000/iss-stream"
+import time
+import requests
+URL = "http://127.0.0.1:5000/query"
 
 
 def main():
-    with httpx.stream("GET", URL, timeout=None) as response:
-        response.raise_for_status()
-        print(f"connected: {response.status_code}")
+    while True:
+        with requests.get(URL,stream=True) as response:
+            response.raise_for_status()
+            print(f"connected: {response.status_code}")
+            for line in response.iter_lines():
+                if not line:
+                    continue
 
-        for line in response.iter_lines():
-            if not line or not line.startswith("data:"):
-                continue
-
-            payload = json.loads(line.removeprefix("data:").strip())
-            print(payload)
+                payload = json.loads(line.strip())
+                print(payload)
+                time.sleep(3)
 
 
 if __name__ == "__main__":
