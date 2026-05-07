c48d71ccd28d34cf43e92260cdb9b537006fac4f
kperera2019 <kevin.perera13@gmail.com>
2026-05-07 16:45:46 -0400

    State tracking of where it's been vs where it is

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import requests
-import httpx
+
 app = Flask(__name__)
 
 QUERY_URL="https://api.wheretheiss.at/v1/satellites/25544"
--- globe.py
+++ globe.py
@@ -1,19 +1,6 @@
 from query import get_iss_position
 from math import cos, sin, radians
-payload = get_iss_position()
-lat = payload["latitude"]
-lon = payload["longitude"]
-
-lat2rad= radians(lat)
-lon2rad = radians(lon)
-
-print(f"ISS is currently at lat: {lat}, lon: {lon}")
-
-print("In globe space, we'd need to account for some basics.")
-
-print("Assuming a globe with a radius of... what, 10 inches?")
-r = 10
-
+import time
 def lat_lon_xyz(lat, lon, r):
     lat2rad= radians(lat)
     lon2rad = radians(lon)
@@ -22,6 +9,45 @@ def lat_lon_xyz(lat, lon, r):
     z = r * sin(lat2rad)
     return x, y, z
 
-print(f"Radius of the globe: {r} inches")
-x, y, z = lat_lon_xyz(lat, lon, r)
-print(f"ISS position in globe space: x={x:.2f}, y={y:.2f}, z={z:.2f}")
+def main():
+    payloads=[]
+    while True:
+        payload = get_iss_position()
+       
+        payloads.append(payload)
+
+        print(f"Current time: {time.ctime().split()[3]} LOCAL - ISS position query:")
+        lat = payload["latitude"]
+        lon = payload["longitude"]
+        vel = payload["velocity"]
+        timestamp= payload["timestamp"]
+
+        print(f"ISS is currently at lat: {lat}, lon: {lon} moving at a speed of {vel} km/h")
+
+
+        if(len(payloads) > 1):
+            print("Previous position:")
+            prev = payloads[-2]
+            print(f"lat: {prev['latitude']}, lon: {prev['longitude']}")
+            print(f"Velocity: {prev['velocity']} km/h")
+            dlon = radians(lon - prev['longitude'])
+            dlat = radians(lat - prev['latitude'])
+            dt = timestamp - prev['timestamp']
+            print(f"Change in position: dlat={dlat:.4f} rad, dlon={dlon:.4f} rad, dt={dt} seconds")
+        else:
+            print("No previous position data available.")
+
+
+        print("In globe space, we'd need to account for some basics.")
+
+        print("Assuming a globe with a radius of... what, 10 inches?")
+        r = 10
+
+        print(f"Radius of the globe: {r} inches")
+        x, y, z = lat_lon_xyz(lat, lon, r)
+        print(f"ISS position in globe space: x={x:.2f}, y={y:.2f}, z={z:.2f}")
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
