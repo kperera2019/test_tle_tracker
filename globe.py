380046665447d7dd03d940f7a1969622d18e6aa3
kperera2019 <kevin.perera13@gmail.com>
2026-05-07 12:56:08 -0400

    Rendering Earth properly

--- globe.py
+++ globe.py
@@ -1,4 +1,11 @@
-from vpython import *
+from vpython import sphere, textures, rate, vector
+from math import radians
 
-# Create the globe
-globe = sphere(pos=vec(0,0,0), radius=1, texture=textures.earth)
+print("Rendering globe")
+
+earth = sphere(pos=vector(0,0,0), radius=1, texture=textures.earth)
+#Rotation optional. for sake of clarity and ease of use, 
+# earth.rotate(angle=radians(23.5), axis=vector(0,0,1))
+while True:
+    rate(60)
+    earth.rotate(angle=0.01, axis=vector(0,1,0))
